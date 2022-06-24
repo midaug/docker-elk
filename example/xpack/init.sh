68ce946b4a2f6226b37478a9e995a13c3fb7e086
MidAug <midaug@midaug.lan>
2022-06-24 14:22:33 +0800

    update

--- example/xpack/init.sh
+++ example/xpack/init.sh
@@ -2,10 +2,16 @@
 DIR="$( cd "$( dirname "$0"  )" && pwd )"
 cd "${DIR}"
 
+apt update && apt install expect
+
 docker-compose stop && docker-compose rm -f
-sudo rm -rf ./elasticsearch/data
-mkdir -p ./elasticsearch/data
+sudo rm -rf ./elasticsearch
+mkdir -p ./elasticsearch
 docker-compose up -d
 sleep 5s
+cp -f ./elasticsearch-base.yml ./elasticsearch/config/elasticsearch.yml
+docker-compose restart
+sleep 5s
 ./setpwd.sh
+docker-compose exec elasticsearch bin/elasticsearch-plugin install https://ghproxy.com/https://github.com/medcl/elasticsearch-analysis-ik/releases/download/v7.1.1/elasticsearch-analysis-ik-7.1.1.zip
 docker-compose restart
